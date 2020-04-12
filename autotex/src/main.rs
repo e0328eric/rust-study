07b0dd4016e202a8fc0074db753411b6d2261efc
Sungbae Jeong <almagest0328@gmail.com>
2020-04-12 16:24:04 +0900

    autotex

--- autotex/src/main.rs
+++ autotex/src/main.rs
@@ -1,5 +1,11 @@
+extern crate signal;
 use std::env;
 use std::process;
+use std::thread;
+use std::time::{Duration, Instant};
+
+use signal::Signal;
+use signal::trap::Trap;
 
 mod engines;
 mod utils;
@@ -15,21 +21,36 @@ fn main() {
     compile_tex(&options, filename, init_time);
 }
 
-fn compile_tex(args_: &Vec<String>, fname: &str, init_time: Vec<std::time::SystemTime>) {
-    let args: Vec<&String> = args_.iter().collect();
+fn compile_tex(args: &Vec<String>, fname: &str, init_time: Vec<std::time::SystemTime>) {
+    let trap_int = Trap::trap(&[Signal::SIGINT]);
+    let now = Instant::now();
     let mut time = init_time;
     let continuous_option = String::from("-v");
-    let args_removed: Vec<&String> = args_.iter()
+    let args_removed: Vec<&String> = args.iter()
         .filter(|x| ***x != continuous_option).collect();
     engines::run_engine(&args_removed, &fname);
-    println!("Press Ctrl+C to finish the program");
-    if args.contains(&&continuous_option) {
+    print_quit_message(&args, &continuous_option);
+    if args.contains(&continuous_option) {
         loop {
             if time != utils::get_file_times(&None) {
                 engines::run_engine(&args_removed, &fname);
-                println!("Press Ctrl+C to finish the program");
+                print_quit_message(&args, &continuous_option);
                 time = utils::get_file_times(&None);
             }
+            thread::sleep(Duration::from_secs(1));
+            match trap_int.wait(now) {
+                Some(_) => {
+                    println!("\nQuitting");
+                    break
+                },
+                None => continue,
+            };
         }
+    }
+}
+
+fn print_quit_message(args: &Vec<String>, continuous_option: &String) {
+    if args.contains(&continuous_option) {
+        println!("Press Ctrl+C to finish the program");
     }
 }
