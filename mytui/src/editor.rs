43cbdf6c3f1f3567f6c5b4f8843921fc433e3ff0
Almagest <almagest0328@gmail.com>
2021-01-10 18:55:02 +0900

    Revert "rust-study"
    This reverts commit 001ce7ad167f0b100c9db9a85bca1d86206556b5.

--- mytui/src/editor.rs
+++ mytui/src/editor.rs
@@ -9,15 +9,8 @@ use termion::event::Key;
 const VERSION: &str = env!("CARGO_PKG_VERSION");
 const STATUS_BG_COLOR: color::Rgb = color::Rgb(239, 239, 239);
 const STATUS_FG_COLOR: color::Rgb = color::Rgb(63, 63, 63);
-const QUIT_TIMES: u8 = 3;
 
-#[derive(PartialEq, Copy, Clone)]
-pub enum SearchDirection {
-    Forward,
-    Backward,
-}
-
-#[derive(Default, Clone)]
+#[derive(Default)]
 pub struct Position {
     pub x: usize,
     pub y: usize,
@@ -44,18 +37,17 @@ pub struct Editor {
     offset: Position,
     document: Document,
     status_message: StatusMessage,
-    quit_times: u8,
 }
 
 impl Editor {
     pub fn default() -> Self {
         let args: Vec<String> = env::args().collect();
-        let mut initial_status =
-            String::from("HELP: Ctrl-F = find | Ctrl-O = open | Ctrl-S = save | Ctrl-Q = quit");
-        let document = if let Some(file_name) = args.get(1) {
-            let doc = Document::open(file_name);
-            if let Ok(doc) = doc {
-                doc
+        let mut initial_status = String::from("HELP: Ctrl-Q = quit");
+        let document = if args.len() > 1 {
+            let file_name = &args[1];
+            let doc = Document::open(&file_name);
+            if doc.is_ok() {
+                doc.unwrap()
             } else {
                 initial_status = format!("ERR: Could not open file: {}", file_name);
                 Document::default()
@@ -71,7 +63,6 @@ impl Editor {
             cursor_position: Position::default(),
             status_message: StatusMessage::from(initial_status),
             offset: Position::default(),
-            quit_times: QUIT_TIMES,
         }
     }
     pub fn run(&mut self) {
@@ -110,32 +101,22 @@ impl Editor {
     fn draw_status_bar(&self) {
         let mut status;
         let width = self.terminal.size().width as usize;
-        let modified_indicator = if self.document.is_dirty() {
-            " (modified)"
-        } else {
-            ""
-        };
-
         let mut file_name = "[No Name]".to_string();
         if let Some(name) = &self.document.file_name {
             file_name = name.clone();
             file_name.truncate(20);
         }
-        status = format!(
-            "{} - {} lines{}",
-            file_name,
-            self.document.len(),
-            modified_indicator
-        );
+        status = format!("{} - {} lines", file_name, self.document.len());
 
         let line_indicator = format!(
             "{}/{}",
             self.cursor_position.y.saturating_add(1),
             self.document.len()
         );
-        #[allow(clippy::integer_arithmetic)]
         let len = status.len() + line_indicator.len();
-        status.push_str(&" ".repeat(width.saturating_sub(len)));
+        if width > len {
+            status.push_str(&" ".repeat(width - len));
+        }
         status = format!("{}{}", status, line_indicator);
         status.truncate(width);
         Terminal::set_fg_color(STATUS_FG_COLOR);
@@ -158,32 +139,7 @@ impl Editor {
     fn process_keypress(&mut self) -> Result<(), std::io::Error> {
         let pressed_key = Terminal::read_key()?;
         match pressed_key {
-            Key::Ctrl('q') => {
-                if self.quit_times > 0 && self.document.is_dirty() {
-                    self.status_message = StatusMessage::from(format!(
-                        "WRANING! File has unsaved changes. Press Ctrl-Q {} more times to qiut.",
-                        self.quit_times
-                    ));
-                    self.quit_times -= 1;
-                    return Ok(());
-                }
-                self.should_quit = true
-            }
-            Key::Ctrl('s') => self.save(),
-            Key::Ctrl('o') => self.open_file(),
-            Key::Ctrl('f') => self.search(),
-
-            Key::Char(c) => {
-                self.document.insert(&self.cursor_position, c);
-                self.move_cursor(Key::Right);
-            }
-            Key::Delete => self.document.delete(&self.cursor_position),
-            Key::Backspace => {
-                if self.cursor_position.x > 0 || self.cursor_position.y > 0 {
-                    self.move_cursor(Key::Left);
-                    self.document.delete(&self.cursor_position);
-                }
-            }
+            Key::Ctrl('q') => self.should_quit = true,
             Key::Up
             | Key::Down
             | Key::Left
@@ -195,81 +151,7 @@ impl Editor {
             _ => (),
         }
         self.scroll();
-        if self.quit_times < QUIT_TIMES {
-            self.quit_times = QUIT_TIMES;
-            self.status_message = StatusMessage::from(String::new());
-        }
         Ok(())
-    }
-
-    fn save(&mut self) {
-        if self.document.file_name.is_none() {
-            let new_name = self.prompt("Save as: ", |_, _, _| {}).unwrap_or(None);
-            if new_name.is_none() {
-                self.status_message = StatusMessage::from("Save aborted.".to_string());
-                return;
-            }
-            self.document.file_name = new_name;
-        }
-
-        if self.document.save().is_ok() {
-            self.status_message = StatusMessage::from("File saved successfully.".to_string());
-        } else {
-            self.status_message = StatusMessage::from("Error writting file!".to_string());
-        }
-    }
-
-    fn open_file(&mut self) {
-        let new_name = if let Some(name) = self.prompt("Open file: ", |_, _, _| {}).unwrap_or(None)
-        {
-            name
-        } else {
-            self.status_message = StatusMessage::from("Open aborted.".to_string());
-            return;
-        };
-
-        if let Ok(doc) = Document::open(&new_name) {
-            self.status_message = StatusMessage::from("File opened successfully.".to_string());
-            self.document = doc;
-        } else {
-            self.status_message = StatusMessage::from("Error reading file!".to_string());
-        }
-    }
-
-    fn search(&mut self) {
-        let old_position = self.cursor_position.clone();
-        let mut direction = SearchDirection::Forward;
-        let query = self
-            .prompt(
-                "Search (ESC to cancel, Arrows to navigate): ",
-                |editor, key, query| {
-                    let mut moved = false;
-                    match key {
-                        Key::Right | Key::Down => {
-                            direction = SearchDirection::Forward;
-                            editor.move_cursor(Key::Right);
-                            moved = true;
-                        }
-                        Key::Left | Key::Up => direction = SearchDirection::Backward,
-                        _ => direction = SearchDirection::Forward,
-                    }
-                    if let Some(position) =
-                        editor
-                            .document
-                            .find(&query, &editor.cursor_position, direction)
-                    {
-                        editor.cursor_position = position;
-                        editor.scroll();
-                    } else if moved {
-                        editor.move_cursor(Key::Left);
-                    }
-                },
-            )
-            .unwrap_or(None);
-        if query.is_none() {
-            self.cursor_position = old_position;
-            self.scroll();
-        }
     }
 
     fn scroll(&mut self) {
@@ -292,7 +174,7 @@ impl Editor {
     fn move_cursor(&mut self, key: Key) {
         let terminal_height = self.terminal.size().height as usize;
         let Position { mut x, mut y } = self.cursor_position;
-        let height = self.document.len();
+        let height = self.document.len() + terminal_height / 2;
         let mut width = if let Some(row) = self.document.row(y) {
             row.len()
         } else {
@@ -327,14 +209,14 @@ impl Editor {
             }
             Key::PageUp => {
                 y = if y > terminal_height {
-                    y.saturating_sub(terminal_height)
+                    y - terminal_height
                 } else {
                     0
                 }
             }
             Key::PageDown => {
                 y = if y.saturating_add(terminal_height) < height {
-                    y.saturating_add(terminal_height)
+                    y + terminal_height as usize
                 } else {
                     height
                 }
@@ -358,7 +240,6 @@ impl Editor {
         let mut welcome_message = format!("Hecto editor -- version {}", VERSION);
         let width = self.terminal.size().width as usize;
         let len = welcome_message.len();
-        #[allow(clippy::integer_arithmetic, clippy::integer_division)]
         let padding = width.saturating_sub(len) / 2;
         let spaces = " ".repeat(padding.saturating_sub(1));
         welcome_message = format!("~{}{}", spaces, welcome_message);
@@ -368,20 +249,16 @@ impl Editor {
     pub fn draw_row(&self, row: &Row) {
         let width = self.terminal.size().width as usize;
         let start = self.offset.x;
-        let end = self.offset.x.saturating_add(width);
+        let end = self.offset.x + width;
         let row = row.render(start, end);
         println!("{}\r", row);
     }
 
-    #[allow(clippy::integer_arithmetic, clippy::integer_division)]
     fn draw_rows(&self) {
         let height = self.terminal.size().height;
         for terminal_row in 0..height {
             Terminal::clear_current_line();
-            if let Some(row) = self
-                .document
-                .row(self.offset.y.saturating_add(terminal_row as usize))
-            {
+            if let Some(row) = self.document.row(terminal_row as usize + self.offset.y) {
                 self.draw_row(row);
             } else if self.document.is_empty() && terminal_row == height / 3 {
                 self.draw_welcome_message();
@@ -389,38 +266,6 @@ impl Editor {
                 println!("~\r");
             }
         }
-    }
-
-    fn prompt<C>(&mut self, prompt: &str, mut callback: C) -> Result<Option<String>, std::io::Error>
-    where
-        C: FnMut(&mut Self, Key, &String),
-    {
-        let mut result = String::new();
-        loop {
-            self.status_message = StatusMessage::from(format!("{}{}", prompt, result));
-            self.refresh_screen()?;
-            let key = Terminal::read_key()?;
-            match key {
-                Key::Backspace => result.truncate(result.len().saturating_sub(1)),
-                Key::Char('\n') => break,
-                Key::Char(c) => {
-                    if !c.is_control() {
-                        result.push(c);
-                    }
-                }
-                Key::Esc => {
-                    result.truncate(0);
-                    break;
-                }
-                _ => (),
-            }
-            callback(self, key, &result);
-        }
-        self.status_message = StatusMessage::from(String::new());
-        if result.is_empty() {
-            return Ok(None);
-        }
-        Ok(Some(result))
     }
 }
 
--- mytui/src/row.rs
+++ mytui/src/row.rs
@@ -1,8 +1,6 @@
-use crate::SearchDirection;
 use std::cmp;
 use unicode_segmentation::UnicodeSegmentation;
 
-#[derive(Default)]
 pub struct Row {
     string: String,
     len: usize,
@@ -10,10 +8,12 @@ pub struct Row {
 
 impl From<&str> for Row {
     fn from(slice: &str) -> Self {
-        Self {
+        let mut row = Self {
             string: String::from(slice),
-            len: slice.graphemes(true).count(),
-        }
+            len: 0,
+        };
+        row.update_len();
+        row
     }
 }
 
@@ -22,7 +22,6 @@ impl Row {
         let end = cmp::min(end, self.string.len());
         let start = cmp::min(start, end);
         let mut result = String::new();
-        #[allow(clippy::integer_arithmetic)]
         for grapheme in self.string[..]
             .graphemes(true)
             .skip(start)
@@ -36,121 +35,13 @@ impl Row {
         }
         result
     }
-
     pub fn len(&self) -> usize {
         self.len
     }
-
     pub fn is_empty(&self) -> bool {
         self.len == 0
     }
-
-    pub fn insert(&mut self, at: usize, c: char) {
-        if at >= self.len() {
-            self.string.push(c);
-            self.len += 1;
-            return;
-        }
-        let mut result = String::new();
-        let mut length = 0;
-        for (index, grapheme) in self.string[..].graphemes(true).enumerate() {
-            length += 1;
-            if index == at {
-                length += 1;
-                result.push(c);
-            }
-            result.push_str(grapheme);
-        }
-        self.len = length;
-        self.string = result;
-    }
-
-    #[allow(clippy::integer_arithmetic)]
-    pub fn delete(&mut self, at: usize) {
-        if at >= self.len() {
-            return;
-        }
-        let mut result = String::new();
-        let mut length = 0;
-        for (index, grapheme) in self.string[..].graphemes(true).enumerate() {
-            if index != at {
-                length += 1;
-                result.push_str(grapheme);
-            }
-        }
-        self.len = length;
-        self.string = result;
-    }
-
-    #[allow(clippy::integer_arithmetic)]
-    pub fn append(&mut self, new: &Self) {
-        self.string = format!("{}{}", self.string, new.string);
-        self.len += new.len;
-    }
-
-    #[allow(clippy::integer_arithmetic)]
-    pub fn split(&mut self, at: usize) -> Self {
-        let mut row = String::new();
-        let mut length = 0;
-        let mut splitted_row = String::new();
-        let mut splitted_length = 0;
-        for (index, grapheme) in self.string[..].graphemes(true).enumerate() {
-            if index < at {
-                length += 1;
-                row.push_str(grapheme);
-            } else {
-                splitted_length += 1;
-                splitted_row.push_str(grapheme);
-            }
-        }
-
-        self.string = row;
-        self.len = length;
-        Self {
-            string: splitted_row,
-            len: splitted_length,
-        }
-    }
-
-    pub fn as_bytes(&self) -> &[u8] {
-        self.string.as_bytes()
-    }
-
-    pub fn find(&self, query: &str, at: usize, direction: SearchDirection) -> Option<usize> {
-        if at > self.len() {
-            return None;
-        }
-        let start = if direction == SearchDirection::Forward {
-            at
-        } else {
-            0
-        };
-        let end = if direction == SearchDirection::Forward {
-            self.len
-        } else {
-            at
-        };
-        #[allow(clippy::integer_arithmetic)]
-        let substring: String = self.string[..]
-            .graphemes(true)
-            .skip(start)
-            .take(end - start)
-            .collect();
-        let matching_byte_index = if direction == SearchDirection::Forward {
-            substring.find(query)
-        } else {
-            substring.rfind(query)
-        };
-        if let Some(matching_byte_index) = matching_byte_index {
-            for (grapheme_index, (byte_index, _)) in
-                substring[..].grapheme_indices(true).enumerate()
-            {
-                if matching_byte_index == byte_index {
-                    #[allow(clippy::integer_arithmetic)]
-                    return Some(start + grapheme_index);
-                }
-            }
-        }
-        None
+    fn update_len(&mut self) {
+        self.len = self.string[..].graphemes(true).count()
     }
 }
