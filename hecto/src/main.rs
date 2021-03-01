0679f6a9f41ba6c463be2fc74ffa3345bca5effd
Almagest <almagest0328@gmail.com>
2021-03-01 21:48:49 +0900

    rust-study

--- hecto/src/main.rs
+++ hecto/src/main.rs
@@ -1,8 +1,14 @@
 mod editor;
+mod terminal;
+mod document;
+mod row;
 
 use editor::Editor;
+pub use editor::Position;
+pub use terminal::Terminal;
+pub use row::Row;
+pub use document::Document;
 
 fn main() {
-    let editor = Editor::default();
-    editor.run();
+    Editor::default().run();
 }
